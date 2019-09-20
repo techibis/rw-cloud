1870f2f31f76d243edd6cb68dc74e210b69cfb0d
root <ibis@techibis.com>
2019-09-20 15:24:03 +0000

    ssl compatible

--- qa/index.js
+++ qa/index.js
@@ -10,118 +10,106 @@ const nodemailer = require('nodemailer');
 const apiKey = process.env.SHOPIFY_API_KEY;
 const apiSecret = process.env.SHOPIFY_API_SECRET;
 const scopes = 'read_products';
-const forwardingAddress = "http://165.22.42.206:3000"; // Replace this with your HTTPS Forwarding address
+const forwardingAddress = "https://sfshopify.com:3001"; // Replace this with your HTTPS Forwarding address
+const fs = require('fs');
+const split = require('split');
+const http = require('http');
+const https = require("https");
 
 app.get('/shopify', (req, res) => {
     const shop = req.query.shop;
     const name = req.query.name;
     const email = req.query.email;
     const sim = req.query.sim;
-    sendmail(name, email, sim);
     if (shop) {
-      const state = nonce();
-      const redirectUri = forwardingAddress + '/shopify/callback';
-      const installUrl = 'https://' + shop +
-        '/admin/oauth/authorize?client_id=' + apiKey +
-        '&scope=' + scopes +
-        '&state=' + state +
-        '&redirect_uri=' + redirectUri;
-  
-      res.cookie('state', state);
-      res.redirect(installUrl);
-    } else {
-      return res.status(400).send('Missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request');
-    }
-});
-
-
-app.get('/shopify/callback', (req, res) => {
-    const { shop, hmac, code, state } = req.query;
-    const stateCookie = cookie.parse(req.headers.cookie).state;
-  
-    if (state !== stateCookie) {
-      return res.status(403).send('Request origin cannot be verified');
-    }
-  
-    if (shop && hmac && code) {
-      // DONE: Validate request is from Shopify
-      const map = Object.assign({}, req.query);
-      delete map['signature'];
-      delete map['hmac'];
-      const message = querystring.stringify(map);
-      const providedHmac = Buffer.from(hmac, 'utf-8');
-      const generatedHash = Buffer.from(
-        crypto
-          .createHmac('sha256', apiSecret)
-          .update(message)
-          .digest('hex'),
-          'utf-8'
-        );
-      let hashEquals = false;
-  
-      try {
-        hashEquals = crypto.timingSafeEqual(generatedHash, providedHmac)
-      } catch (e) {
-        hashEquals = false;
-      };
-  
-      if (!hashEquals) {
-        return res.status(400).send('HMAC validation failed');
+      if (shop != "republicwireless-test.myshopify.com") 
+        return res.status(400).send('Invalid parameter. Please add the appropriate parameters to your request');
+      else {
+          if (sim) {
+            const state = nonce();
+            res.cookie('state', state);
+	    checksim(sim, res);
+          } else {
+            return res.status(400).send('Missing parameter. Please add the appropriate parameters to your request');
+          }
       }
-  
-      // DONE: Exchange temporary code for a permanent access token
-      const accessTokenRequestUrl = 'https://' + shop + '/admin/oauth/access_token';
-      const accessTokenPayload = {
-        client_id: apiKey,
-        client_secret: apiSecret,
-        code,
-      };
-  
-      request.post(accessTokenRequestUrl, { json: accessTokenPayload })
-      .then((accessTokenResponse) => {
-        const accessToken = accessTokenResponse.access_token;
-        // DONE: Use access token to make API call to 'shop' endpoint
-        const shopRequestUrl = 'https://' + shop + '/admin/api/2019-07/shop.json';
-        const shopRequestHeaders = {
-          'X-Shopify-Access-Token': accessToken,
-        };
-  
-        request.get(shopRequestUrl, { headers: shopRequestHeaders })
-        .then((shopResponse) => {
-          res.status(200).end(shopResponse);
-        })
-        .catch((error) => {
-          res.status(error.statusCode).send(error.error.error_description);
-        });
-      })
-      .catch((error) => {
-        res.status(error.statusCode).send(error.error.error_description);
-      });
-  
     } else {
-      res.status(400).send('Required parameters missing');
+      return res.status(400).send('Missing parameter. Please add the appropriate parameters to your request');
     }
 });
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/sfshopify.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/sfshopify.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/sfshopify.com/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(3000, () => {
+	console.log('Sim Validation App listening on port 3000 for HTTP Server');
+});
+
+httpsServer.listen(3001, () => {
+	console.log('Sim Validation App listening on port 3001 for HTTPS Server');
 });
 
 function sendmail(name, email, sim) {
 
-let transporter = nodemailer.createTransport({
-    sendmail: true,
-    newline: 'unix',
-    path: '/usr/sbin/sendmail'
-});
-transporter.sendMail({
-    from: 'sim@republicwireless.com',
-    to: 'ibis@softwarefactoryexperts.com',
-    subject: 'Sim Activation',
-    text: 'SIM: ' + sim + '\nNAME: ' + name + '\nEMAIL: ' + email
-}, (err, info) => {
-    console.log(info.envelope);
-    console.log(info.messageId);
-});
+    let transporter = nodemailer.createTransport({
+        sendmail: true,
+        newline: 'unix',
+        path: '/usr/sbin/sendmail' 
+    });
+    transporter.sendMail({
+        from: 'sim@republicwireless.com',
+        to: 'ibis@softwarefactoryexperts.com',
+        subject: 'Sim Activation',
+        text: 'SIM: ' + sim + '\nNAME: ' + name + '\nEMAIL: ' + email
+    }, (err, info) => {
+        console.log(info.envelope);
+        console.log(info.messageId);
+    });
 
 }
+
+function checksim(sim, res) {
+    var list = [];
+    var read = fs.createReadStream('iccid_data_sim.csv')
+    .pipe(split())
+    .on('data',function(data){
+       list.push(data);
+    })
+    .on('end', function(data){
+       console.log('Read finished');
+       console.log(list);
+       validateCsvRow(sim, list, res);
+    })
+}
+
+function validateCsvRow(simNo, list, res) {
+   let found = false;
+   for (var i = 0; i < list.length; i++) {
+	console.log("comparing " + simNo + " and " + list[i]);
+      if (simNo==list[i]) {
+        console.log ("valid number");
+        found = true;
+      } else {
+         console.log ("invalid number");
+      }
+   }
+    console.log("found? " + found);
+    if (found) {
+       sendmail(name, email, sim);
+       return res.status(200).send('{"valid": "true"}');
+    } else {
+       return res.status(200).send('{"valid": "false"}');
+    }
+
+}
+
